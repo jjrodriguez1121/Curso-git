64eca494d5628ef9f6449016884aa886f3a445cb
Jeferson Rodriguez Jaime <145308969+jjrodriguez1121@users.noreply.github.com>
2025-12-11 23:06:32 -0500

    Subiendo archivo de python

---
+++ func.py
@@ -0,0 +1,16 @@
+def suma (a,b,c=0, d=0):
+    if c!=0 and d!=0:
+        return a+b+c+d
+    elif c!=0:
+        return a+b+c
+    else:
+        return a+b  
+    
+def resta (a,b):
+    return a-b
+
+def multiplicacion (a,b):
+    return a*b
+
+def division (a,b):
+    return a/b
